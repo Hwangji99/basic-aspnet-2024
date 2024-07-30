65d2a58e3f80495b9ed4fb26f7dbea66c90dc91c
Hwangji99 <ghkdwl1303@naver.com>
2024-07-30 12:25:18 +0900

    Update ContactController.cs

--- day10/MyPortfolio/Controllers/ContactController.cs
+++ day10/MyPortfolio/Controllers/ContactController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Mail;
 
 namespace MyPortfolio.Controllers
 {
@@ -10,6 +11,51 @@ namespace MyPortfolio.Controllers
         {
             return View();
         }
-        
+
+        [HttpPost]
+        public ActionResult SendMail()
+        {
+            try
+            {
+                MailMessage mailMessage = new MailMessage();
+
+                mailMessage.From = new MailAddress("보내는 계정 주소", "표시 이름", System.Text.Encoding.UTF8);
+                // 받는이 메일 주소
+                mailMessage.To.Add("yyy@naver.com");
+                // 참조 메일 주소
+                mailMessage.CC.Add("zzz@naver.com");
+                // 비공개 참조 메일 주소
+                mailMessage.Bcc.Add("kkk@naver.com");
+                // 제목
+                mailMessage.Subject = "메일 제목";
+                // 메일 제목 인코딩 타입(UTF-8) 선택
+                mailMessage.SubjectEncoding = System.Text.Encoding.UTF8;
+                // 본문
+                mailMessage.Body = "메일 본문";
+                // 본문의 포맷에 따라 선택
+                mailMessage.IsBodyHtml = false;
+                // 본문 인코딩 타입(UTF-8) 선택
+                mailMessage.BodyEncoding = System.Text.Encoding.UTF8;
+                // 파일 첨부
+                mailMessage.Attachments.Add(new Attachment(new FileStream(@"D:\test.zip", FileMode.Open, FileAccess.Read), "test.zip"));
+                // SMTP 서버 주소
+                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+                // SMTP 포트
+                SmtpServer.Port = 587;
+                // SSL 사용 여부
+                SmtpServer.EnableSsl = true;
+                SmtpServer.UseDefaultCredentials = false;
+                SmtpServer.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
+                SmtpServer.Credentials = new System.Net.NetworkCredential("아이디", "패스워드");
+
+                SmtpServer.Send(mailMessage);
+            }
+            catch (Exception ex)
+            {
+                // 에러메시지 HTML
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
